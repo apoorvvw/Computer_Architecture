4fc80272d491a1af0ed5ec137a9ca00d5ae7cb3c
Apoorv Wairagade <awairaga@purdue.edu>
2016-09-07 23:24:07 -0400

    Lab 3

--- source/memory_control.sv
+++ source/memory_control.sv
@@ -22,4 +22,36 @@ module memory_control (
   // number of cpus for cc
   parameter CPUS = 2;
 
+  assign ccif.iwait = (ccif.ramstate == ACCESS) ? ((ccif.iREN) && (!ccif.dREN && !ccif.dWEN) ? 0 : 1) : 1;
+  assign ccif.dwait = (ccif.ramstate == ACCESS) ? ((ccif.dREN || ccif.dWEN) ? 0 : 1) : 1;
+
+  always_comb begin
+    
+    ccif.ramaddr = '0;
+    ccif.ramWEN = 0;
+    ccif.ramREN = 0;
+    ccif.ramstore = 0;
+    ccif.dload = 0;
+    ccif.iload = 0;
+
+    if (ccif.dWEN) begin
+      ccif.ramaddr = ccif.daddr;
+      ccif.ramWEN = 1;
+      ccif.ramREN = 0;
+      ccif.ramstore = ccif.dstore;
+    end
+    else if (ccif.dREN) begin
+      ccif.ramaddr = ccif.daddr;
+      ccif.ramWEN = 0;
+      ccif.ramREN = 1;
+      ccif.dload = ccif.ramload;
+    end
+    else if (ccif.iREN) begin
+      ccif.ramaddr = ccif.iaddr;
+      ccif.ramWEN = 0;
+      ccif.ramREN = 1;
+      ccif.iload = ccif.ramload;
+    end
+  end
+
 endmodule
